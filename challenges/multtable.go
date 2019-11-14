37701a4e605a295194152787178720d5cb228f4f
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-14 12:18:12 +0530

    Finished challenge 5

---
+++ challenges/multtable.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter a number (n): ")
+	n, _ := reader.ReadString('\n')
+	n = strings.TrimSuffix(n, "\n")
+	n_num, _ := strconv.Atoi(n)
+
+	for i := 1; i <= 12; i++ {
+		fmt.Printf("%v X %v : %v \n", n_num, i, n_num*i)
+	}
+}
