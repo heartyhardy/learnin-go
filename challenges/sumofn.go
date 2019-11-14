23a68f509df2be41df54a335c58a31142265a6aa
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-14 11:53:36 +0530

    Finished challenge 4

--- challenges/sumofn.go
+++ challenges/sumofn.go
@@ -1,30 +1,32 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter a number: ")
-	text, _ := reader.ReadString('\n')
+// func main() {
+// 	reader := bufio.NewReader(os.Stdin)
+// 	fmt.Print("Enter a number: ")
+// 	text, _ := reader.ReadString('\n')
 
-	text = strings.TrimSuffix(text, "\n")
+// 	text = strings.TrimSuffix(text, "\n")
 
-	num, err := strconv.Atoi(text)
+// 	num, err := strconv.Atoi(text)
 
-	if err != nil {
-		fmt.Println("Invalid Input!")
-	} else {
-		sum := 0
-		for i := 0; i <= num; i++ {
-			sum += i
-		}
+// 	if err != nil {
+// 		fmt.Println("Invalid Input!")
+// 	} else {
+// 		sum := 0
+// 		for i := 0; i <= num; i++ {
+// 			if i%3 == 0 || i%5 == 0 {
+// 				sum += i
+// 			}
+// 		}
 
-		fmt.Printf("\nSum of numbers from 1 to %v is %v \n", num, sum)
-	}
-}
+// 		fmt.Printf("\nSum of numbers from 1 to %v is %v \n", num, sum)
+// 	}
+// }
---
+++ challenges/sumorproduct.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number (n): ")
+
+	n, _ := reader.ReadString('\n')
+	n = strings.TrimSuffix(n, "\n")
+	n_num, _ := strconv.Atoi(n)
+
+	fmt.Print("Enter operation 1 - SUM | 2 - PRODUCT : ")
+
+	ops, _ := reader.ReadString('\n')
+	ops = strings.TrimSuffix(ops, "\n")
+	ops_num, _ := strconv.Atoi(ops)
+
+	sum := 0
+	for i := 0; i <= n_num; i++ {
+		switch ops_num {
+		case 1:
+			sum += i
+		case 2:
+			if sum == 0 {
+				sum = 1
+			}
+			sum *= i
+		}
+	}
+
+	fmt.Printf("Result of the selected operation is: %v \n", sum)
+}
