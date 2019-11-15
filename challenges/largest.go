40943d3952a49253d77b2865d0caa04937db38d7
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-15 22:15:21 +0530

    Added max number -9

---
+++ challenges/largest.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func max(arr []int) int {
+	max_num := 0
+	for i := 0; i < len(arr); i++ {
+		if max_num < arr[i] {
+			max_num = arr[i]
+		}
+	}
+	return max_num
+}
+
+func main() {
+	arr := []int{1, 15, 7, 2, 1, 5, 4, 11, 2, 3}
+	fmt.Println(max(arr))
+}
--- challenges/leap.go
+++ challenges/leap.go
@@ -1,17 +1,17 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	leapyears := []int{}
+// func main() {
+// 	leapyears := []int{}
 
-	for y := 2019; y < 2019+20; y++ {
-		if (y%400 == 0 || y%100 != 0) && y%4 == 0 {
-			leapyears = append(leapyears, y)
-		}
-	}
+// 	for y := 2019; y < 2019+20; y++ {
+// 		if (y%400 == 0 || y%100 != 0) && y%4 == 0 {
+// 			leapyears = append(leapyears, y)
+// 		}
+// 	}
 
-	fmt.Println(leapyears)
-}
+// 	fmt.Println(leapyears)
+// }
