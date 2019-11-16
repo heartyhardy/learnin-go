52318c157b8f9d1a1d6afa103175786b1918a62f
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-16 13:30:28 +0530

    Reverse array - 10

--- challenges/largest.go
+++ challenges/largest.go
@@ -1,20 +1,20 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func max(arr []int) int {
-	max_num := 0
-	for i := 0; i < len(arr); i++ {
-		if max_num < arr[i] {
-			max_num = arr[i]
-		}
-	}
-	return max_num
-}
+// func max(arr []int) int {
+// 	max_num := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if max_num < arr[i] {
+// 			max_num = arr[i]
+// 		}
+// 	}
+// 	return max_num
+// }
 
-func main() {
-	arr := []int{1, 15, 7, 2, 1, 5, 4, 11, 2, 3}
-	fmt.Println(max(arr))
-}
+// func main() {
+// 	arr := []int{1, 15, 7, 2, 1, 5, 4, 11, 2, 3}
+// 	fmt.Println(max(arr))
+// }
---
+++ challenges/reverse.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func reverse(arr []int) []int {
+	for i := 0; i < len(arr)/2; i++ {
+		a := arr[i]
+		b := arr[len(arr)-i-1]
+
+		arr[i] = b
+		arr[len(arr)-i-1] = a
+	}
+
+	return arr
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 7, 2, 1}
+	rarr := reverse(arr)
+
+	fmt.Println(rarr)
+}
