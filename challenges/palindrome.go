2aee08bc2db8d8d589306890d6e1231928a17f54
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-16 22:57:15 +0530

    Is palindrome -14

---
+++ challenges/palindrome.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(word string) bool {
+	arr := []byte(strings.ToLower(word))
+	res := false
+
+	for i := 0; i < len(arr)/2; i++ {
+		if arr[i] != arr[len(arr)-1-i] {
+			res = false
+			break
+		} else {
+			res = true
+		}
+	}
+	return res
+}
+
+func main() {
+	res := isPalindrome("Madam")
+	fmt.Printf("Is palindrome: %v\n", res)
+}
--- challenges/sumofall.go
+++ challenges/sumofall.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func sumofall(arr []int) int {
-	sum := 0
-	for i := 0; i < len(arr)/2; i++ {
-		sum += arr[i] + arr[len(arr)-1-i]
-	}
-	if len(arr)%2 == 1 {
-		sum += arr[len(arr)/2]
-	}
+// func sumofall(arr []int) int {
+// 	sum := 0
+// 	for i := 0; i < len(arr)/2; i++ {
+// 		sum += arr[i] + arr[len(arr)-1-i]
+// 	}
+// 	if len(arr)%2 == 1 {
+// 		sum += arr[len(arr)/2]
+// 	}
 
-	return sum
-}
+// 	return sum
+// }
 
-func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7}
-	sum := sumofall(arr)
+// func main() {
+// 	arr := []int{1, 2, 3, 4, 5, 6, 7}
+// 	sum := sumofall(arr)
 
-	fmt.Println(sum)
-}
+// 	fmt.Println(sum)
+// }
