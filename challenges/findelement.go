5262f2a96d2d72cf270eeffbae997506a7be18a4
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-16 18:24:42 +0530

    Find element -11

---
+++ challenges/findelement.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func find(el int, arr []int) (int, bool) {
+	i := 0
+	found := false
+	for ; i <= len(arr)/2; i++ {
+		if arr[i] == el || arr[len(arr)-1-i] == el {
+			if arr[len(arr)-1-i] == el {
+				i = len(arr) - 1 - i
+			}
+			found = true
+			break
+		}
+	}
+
+	return i, found
+}
+
+func main() {
+	arr := []int{1, 5, 2, 3, 4, 8, 7, 1}
+	el := 2
+
+	pos, res := find(el, arr)
+	fmt.Printf("Result: %v %v \n", res, pos)
+}
--- challenges/reverse.go
+++ challenges/reverse.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func reverse(arr []int) []int {
-	for i := 0; i < len(arr)/2; i++ {
-		a := arr[i]
-		b := arr[len(arr)-i-1]
+// func reverse(arr []int) []int {
+// 	for i := 0; i < len(arr)/2; i++ {
+// 		a := arr[i]
+// 		b := arr[len(arr)-i-1]
 
-		arr[i] = b
-		arr[len(arr)-i-1] = a
-	}
+// 		arr[i] = b
+// 		arr[len(arr)-i-1] = a
+// 	}
 
-	return arr
-}
+// 	return arr
+// }
 
-func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 7, 2, 1}
-	rarr := reverse(arr)
+// func main() {
+// 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 7, 2, 1}
+// 	rarr := reverse(arr)
 
-	fmt.Println(rarr)
-}
+// 	fmt.Println(rarr)
+// }
