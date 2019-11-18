40fdeec05e300ee2e623644e03bee465a2a002d0
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-18 20:41:13 +0530

    Merging with sorting

--- challenges/concatarr2.go
+++ challenges/concatarr2.go
@@ -1,27 +1,27 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func concat(arr1 []int, arr2 []int) []int {
-	maxlen := len(arr1) + len(arr2) - 1
-	newarr := []int{}
+// func concat(arr1 []int, arr2 []int) []int {
+// 	maxlen := len(arr1) + len(arr2) - 1
+// 	newarr := []int{}
 
-	for i := 0; i < maxlen; i++ {
-		if i < len(arr1) {
-			newarr = append(newarr, arr1[i])
-		}
-		if i < len(arr2) {
-			newarr = append(newarr, arr2[i])
-		}
-	}
+// 	for i := 0; i < maxlen; i++ {
+// 		if i < len(arr1) {
+// 			newarr = append(newarr, arr1[i])
+// 		}
+// 		if i < len(arr2) {
+// 			newarr = append(newarr, arr2[i])
+// 		}
+// 	}
 
-	return newarr
-}
+// 	return newarr
+// }
 
-func main() {
-	arr1 := []int{1, 2, 3, 4, 5, 6}
-	arr2 := []int{10, 20, 30, 40, 50, 60}
+// func main() {
+// 	arr1 := []int{1, 2, 3, 4, 5, 6}
+// 	arr2 := []int{10, 20, 30, 40, 50, 60}
 
-	newarr := concat(arr1, arr2)
-	fmt.Println(newarr)
-}
+// 	newarr := concat(arr1, arr2)
+// 	fmt.Println(newarr)
+// }
