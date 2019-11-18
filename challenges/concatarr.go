319ba944d6faca0eba0b57d53f5902a1dbecef2f
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-18 14:12:09 +0530

    Concat array pick from each

--- challenges/concatarr.go
+++ challenges/concatarr.go
@@ -1,29 +1,29 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func concat(arr1 []int, arr2 []int) []int {
-	newarr := []int{}
-	maxlen := len(arr1) + len(arr2) - 1
+// func concat(arr1 []int, arr2 []int) []int {
+// 	newarr := []int{}
+// 	maxlen := len(arr1) + len(arr2) - 1
 
-	for i := 0; i <= maxlen; i++ {
-		if i < len(arr1) {
-			newarr = append(newarr, arr1[i])
-		} else {
-			newarr = append(newarr, arr2[i-len(arr1)])
-		}
-	}
+// 	for i := 0; i <= maxlen; i++ {
+// 		if i < len(arr1) {
+// 			newarr = append(newarr, arr1[i])
+// 		} else {
+// 			newarr = append(newarr, arr2[i-len(arr1)])
+// 		}
+// 	}
 
-	return newarr
-}
+// 	return newarr
+// }
 
-func main() {
-	arr1 := []int{1, 2, 3, 4}
-	arr2 := []int{10, 20, 30, 40}
+// func main() {
+// 	arr1 := []int{1, 2, 3, 4}
+// 	arr2 := []int{10, 20, 30, 40}
 
-	newarr := concat(arr1, arr2)
+// 	newarr := concat(arr1, arr2)
 
-	fmt.Println(newarr)
-}
+// 	fmt.Println(newarr)
+// }
