6ac5eb18eabfd204b1758bd83ce64ccbe86651e0
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-20 01:56:55 +0530

    Fixing bugs in Rotate array

--- challenges/rotatearr.go
+++ challenges/rotatearr.go
@@ -4,6 +4,12 @@ import "fmt"
 
 func rotate(arr []int, n int) {
 
+	if n >= len(arr) {
+		panic("n cannot be larger than the length of the given array!")
+	} else if n < 0 {
+		panic("n cannot be smaller than 0!")
+	}
+
 	whead := 0
 	rhead := n
 
@@ -16,13 +22,14 @@ func rotate(arr []int, n int) {
 		arr[whead] = r
 		arr[rhead] = w
 
+		fmt.Printf("%v \n", arr)
+
 		rhead++
+		whead++
 
 		if rhead == len(arr) {
-			rhead = 0
+			break
 		}
-
-		whead++
 	}
 
 	return
@@ -32,5 +39,5 @@ func main() {
 	arr := []int{1, 2, 3, 4, 5, 6}
 	rotate(arr, 2)
 
-	fmt.Println(arr)
+	//fmt.Println(arr)
 }
