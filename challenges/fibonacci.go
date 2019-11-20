faa77b23f27173c96c08ca24cf4866b920a68e84
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-20 08:14:09 +0530

    Fibonacci with memoization

---
+++ challenges/fibonacci.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+var fibo_cache = make(map[uint64]uint64)
+
+func fibogen(n uint64) uint64 {
+	// Generate Fibonacci sequence
+	// Use memoization
+
+	if fibo_cache[n] != 0 {
+		return fibo_cache[n]
+	}
+
+	var value uint64 = 0
+
+	if n == 1 {
+		value = 1
+	} else if n == 2 {
+		value = 1
+	} else {
+		value = fibogen(n-1) + fibogen(n-2)
+	}
+
+	fibo_cache[n] = value
+	return value
+}
+
+func main() {
+	var i uint64 = 0
+	for i = 0; i < 100; i++ {
+		fmt.Println(fibogen(i + 1))
+	}
+}
--- challenges/rotatearr.go
+++ challenges/rotatearr.go
@@ -1,43 +1,43 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func rotate(arr []int, n int) {
+// func rotate(arr []int, n int) {
 
-	if n >= len(arr) {
-		panic("n cannot be larger than the length of the given array!")
-	} else if n < 0 {
-		panic("n cannot be smaller than 0!")
-	}
+// 	if n >= len(arr) {
+// 		panic("n cannot be larger than the length of the given array!")
+// 	} else if n < 0 {
+// 		panic("n cannot be smaller than 0!")
+// 	}
 
-	whead := 0
-	rhead := n
+// 	whead := 0
+// 	rhead := n
 
-	w, r := 0, 0
+// 	w, r := 0, 0
 
-	for whead <= len(arr)-1 {
-		w = arr[whead]
-		r = arr[rhead]
+// 	for whead <= len(arr)-1 {
+// 		w = arr[whead]
+// 		r = arr[rhead]
 
-		arr[whead] = r
-		arr[rhead] = w
+// 		arr[whead] = r
+// 		arr[rhead] = w
 
-		fmt.Printf("%v \n", arr)
+// 		fmt.Printf("%v \n", arr)
 
-		rhead++
-		whead++
+// 		rhead++
+// 		whead++
 
-		if rhead == len(arr) {
-			break
-		}
-	}
+// 		if rhead == len(arr) {
+// 			break
+// 		}
+// 	}
 
-	return
-}
+// 	return
+// }
 
-func main() {
-	arr := []int{1, 2, 3, 4, 5, 6}
-	rotate(arr, 2)
+// func main() {
+// 	arr := []int{1, 2, 3, 4, 5, 6}
+// 	rotate(arr, 2)
 
-	//fmt.Println(arr)
-}
+// 	//fmt.Println(arr)
+// }
