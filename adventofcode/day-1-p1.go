ff2006abcb7e96dd8b2d9b0db95fd7f4e944d2bd
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-01 15:48:10 +0530

    Added advent of code challenges day 1

---
+++ adventofcode/day-1-p1.go
@@ -0,0 +1,41 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"math"
+// 	"strconv"
+// 	"strings"
+// )
+
+// func getstrings() []string {
+// 	data, err := ioutil.ReadFile("day-1-input.txt")
+// 	if err != nil {
+// 		print("Reading data file failed. Cannot skip this criical operation.")
+// 		panic("Quitting")
+// 	}
+
+// 	moduleMasses := strings.Split(string(data), "\n")
+
+// 	return moduleMasses
+// }
+
+// func calModuleMass(modules []string) int {
+// 	fuelreq := 0
+// 	for i := 0; i < len(modules); i++ {
+// 		mass, err := strconv.Atoi(modules[i])
+// 		if err != nil {
+// 			print("Invalid data format!")
+// 			panic("Quitting")
+// 		}
+
+// 		fuelreq += int(math.Floor(float64(mass/3))) - 2
+// 	}
+// 	return fuelreq
+// }
+
+// func main() {
+// 	modules := getstrings()
+// 	fuelreq := calModuleMass(modules)
+// 	fmt.Println(fuelreq)
+// }
--- challengesII/matrix-cells-odds.go
+++ challengesII/matrix-cells-odds.go
@@ -1,19 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func oddCells(n int, m int, indices [][]int) int {
+
+	if n == 0 || m == 0 || len(indices[0]) < 1 {
+
+		return 0
+	}
+
+	odds := 0
 	matrix := make([][]int, n)
-	l := 0
-	for i := 0; i < n; i++ {
+
+	for i := 0; i < n; i++ { // O(n)
 		matrix[i] = make([]int, m)
 		for j := 0; j < m; j++ {
-			l++
+			matrix[i][j] = 0
 		}
 	}
-	return l
+
+	for i := 0; i < len(indices); i++ {
+		for k := 0; k < n; k++ { // O(n)
+			for j := 0; j < m; j++ {
+				if k == indices[i][0] {
+					matrix[k][j] = matrix[k][j] + 1
+				}
+				if j == indices[i][1] {
+					matrix[k][j] = matrix[k][j] + 1
+				}
+			}
+		}
+	}
+
+	for k := 0; k < n; k++ { // O(n)
+		for j := 0; j < m; j++ {
+			if matrix[k][j]%2 != 0 {
+				odds++
+			}
+		}
+	}
+
+	return odds
 }
 
 func main() {
-	fmt.Println(oddCells(2, 3, nil))
+	a := [][]int{[]int{}, []int{}}
+	fmt.Println(oddCells(2, 3, a))
 }
