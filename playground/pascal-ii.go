91a15d44508217ac694d40965be4cacc125b7021
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-05 19:12:10 +0530

    Advent of code d5

--- playground/pascal-ii.go
+++ playground/pascal-ii.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func getRow(rowIndex int) []int {
-	row := []int{1}
-	for c := 1; c <= rowIndex; c++ {
-		row = append(row, row[c-1]*(rowIndex-(c-1))/c)
-	}
-	return row
-}
+// func getRow(rowIndex int) []int {
+// 	row := []int{1}
+// 	for c := 1; c <= rowIndex; c++ {
+// 		row = append(row, row[c-1]*(rowIndex-(c-1))/c)
+// 	}
+// 	return row
+// }
 
-func main() {
-	res := getRow(5)
-	fmt.Println(res)
-}
+// func main() {
+// 	res := getRow(5)
+// 	fmt.Println(res)
+// }
