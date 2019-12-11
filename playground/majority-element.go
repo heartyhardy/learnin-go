7f7a687ab700932ebdeccb8ef767d46ffbb16391
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-11 05:58:12 +0530

    Majority element better solution

--- playground/majority-element.go
+++ playground/majority-element.go
@@ -4,26 +4,23 @@ import "fmt"
 
 func majorityElement(nums []int) int {
 	nummap := map[int]int{}
+	maxval, max := 0, 0
 	for _, num := range nums {
 		if _, ok := nummap[num]; !ok {
 			nummap[num] = 1
 		} else {
 			nummap[num] = nummap[num] + 1
 		}
-	}
-
-	maxval, max := 0, 0
-	for k, v := range nummap {
-		if maxval < v {
-			maxval = v
-			max = k
+		if maxval < nummap[num] {
+			maxval = nummap[num]
+			max = num
 		}
 	}
 	return max
 }
 
 func main() {
-	a := []int{0, -1, -1, 0}
+	a := []int{0, 3, 3, 2, 1, 3, 3, 3, 3, 1, 1, 2, 2, 2, 2}
 	res := majorityElement(a)
 	fmt.Println(res)
 }
--- playground/test-linkedlist.go
+++ playground/test-linkedlist.go
@@ -1,27 +1,27 @@
 package main
 
-import (
-	"fmt"
-	. "heartyhardy/packages/linkedlist"
-)
+// import (
+// 	"fmt"
+// 	. "heartyhardy/packages/linkedlist"
+// )
 
-func main() {
+// func main() {
 
-	var node Node = Node{
-		Value: 10,
-		Next:  nil,
-	}
+// 	var node Node = Node{
+// 		Value: 10,
+// 		Next:  nil,
+// 	}
 
-	var node2 Node = Node{
-		Value: 20,
-	}
+// 	var node2 Node = Node{
+// 		Value: 20,
+// 	}
 
-	var ll LinkedLister = &LinkedList{}
+// 	var ll LinkedLister = &LinkedList{}
 
-	ll.Append(&node)
-	ll.Append(&node2)
-	end := ll.Peek()
+// 	ll.Append(&node)
+// 	ll.Append(&node2)
+// 	end := ll.Peek()
 
-	fmt.Println(end)
+// 	fmt.Println(end)
 
-}
+// }
