1d6166aa9a55504413c1af66fb1b9f4e9aa2095e
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-13 14:18:59 +0530

    Advent of code day 13

--- adventofcode/day-11-p1.go
+++ adventofcode/day-11-p1.go
@@ -1,166 +1,166 @@
 package main
 
-import (
-	"fmt"
-	"heartyhardy/ivm"
-)
+// import (
+// 	"fmt"
+// 	"heartyhardy/ivm"
+// )
 
-var count int
+// var count int
 
-const (
-	black = iota
-	white
-)
+// const (
+// 	black = iota
+// 	white
+// )
 
-const (
-	paintReady = iota
-	moveReady
-)
+// const (
+// 	paintReady = iota
+// 	moveReady
+// )
 
-type Vector struct {
-	x int
-	y int
-}
+// type Vector struct {
+// 	x int
+// 	y int
+// }
 
-func (v *Vector) Flip() *Vector {
-	t := v.x
-	v.x = v.y
-	v.y = t
-	return v
-}
+// func (v *Vector) Flip() *Vector {
+// 	t := v.x
+// 	v.x = v.y
+// 	v.y = t
+// 	return v
+// }
 
-func (v *Vector) Advance(w *Vector) *Vector {
-	v.x = v.x + w.x
-	v.y = v.y + w.y
-	return v
-}
+// func (v *Vector) Advance(w *Vector) *Vector {
+// 	v.x = v.x + w.x
+// 	v.y = v.y + w.y
+// 	return v
+// }
 
-//Flip key and multiply by value
-var directions map[Vector]Vector = map[Vector]Vector{
-	Vector{0, 1}:  Vector{-1, 1},
-	Vector{-1, 0}: Vector{1, -1},
-	Vector{0, -1}: Vector{-1, 1},
-	Vector{1, 0}:  Vector{1, -1},
-}
+// //Flip key and multiply by value
+// var directions map[Vector]Vector = map[Vector]Vector{
+// 	Vector{0, 1}:  Vector{-1, 1},
+// 	Vector{-1, 0}: Vector{1, -1},
+// 	Vector{0, -1}: Vector{-1, 1},
+// 	Vector{1, 0}:  Vector{1, -1},
+// }
 
-type Panel struct {
-	x int
-	y int
-}
+// type Panel struct {
+// 	x int
+// 	y int
+// }
 
-type Painter interface {
-	Paint(int)
-}
+// type Painter interface {
+// 	Paint(int)
+// }
 
-type Mover interface {
-	Move(int) int
-}
+// type Mover interface {
+// 	Move(int) int
+// }
 
-type Executor interface {
-	Execute(ivm.VM) func()
-}
+// type Executor interface {
+// 	Execute(ivm.VM) func()
+// }
 
-type Receiver interface {
-	Receive(int)
-}
+// type Receiver interface {
+// 	Receive(int)
+// }
 
-type Robot struct {
-	messages  []int
-	panels    map[Panel]int
-	state     int
-	direction *Vector
-	location  *Vector
-}
+// type Robot struct {
+// 	messages  []int
+// 	panels    map[Panel]int
+// 	state     int
+// 	direction *Vector
+// 	location  *Vector
+// }
 
-func (rb *Robot) Paint(color int) {
-	panel := Panel{x: rb.location.x, y: rb.location.y}
-	if _, ok := rb.panels[panel]; ok {
-		rb.panels[panel] = color
-		rb.state = moveReady
-	}
-}
+// func (rb *Robot) Paint(color int) {
+// 	panel := Panel{x: rb.location.x, y: rb.location.y}
+// 	if _, ok := rb.panels[panel]; ok {
+// 		rb.panels[panel] = color
+// 		rb.state = moveReady
+// 	}
+// }
 
-func (rb *Robot) Move(direction int) {
-	if d, ok := rb.Turn(direction); ok {
-		pos := rb.location.Advance(d)
-		panel := Panel{x: pos.x, y: pos.y}
+// func (rb *Robot) Move(direction int) {
+// 	if d, ok := rb.Turn(direction); ok {
+// 		pos := rb.location.Advance(d)
+// 		panel := Panel{x: pos.x, y: pos.y}
 
-		if _, exists := rb.panels[panel]; !exists {
-			rb.panels[panel] = black
-		}
-		rb.state = paintReady
-	}
-}
+// 		if _, exists := rb.panels[panel]; !exists {
+// 			rb.panels[panel] = black
+// 		}
+// 		rb.state = paintReady
+// 	}
+// }
 
-func (rb *Robot) Execute(setup *ivm.VM) func() {
-	return ivm.Run(setup)
-}
+// func (rb *Robot) Execute(setup *ivm.VM) func() {
+// 	return ivm.Run(setup)
+// }
 
-//Works as the input feed for VM
-func (rb *Robot) Feed() []int {
-	color := black
-	standing := Panel{x: rb.location.x, y: rb.location.y}
-	if _, ok := rb.panels[standing]; ok {
-		color = rb.panels[standing]
-	}
-	rb.messages = append(rb.messages, color)
-	return rb.messages
-}
+// //Works as the input feed for VM
+// func (rb *Robot) Feed() []int {
+// 	color := black
+// 	standing := Panel{x: rb.location.x, y: rb.location.y}
+// 	if _, ok := rb.panels[standing]; ok {
+// 		color = rb.panels[standing]
+// 	}
+// 	rb.messages = append(rb.messages, color)
+// 	return rb.messages
+// }
 
-func (rb *Robot) Receive(message int) {
+// func (rb *Robot) Receive(message int) {
 
-	switch rb.state {
-	case paintReady:
-		rb.Paint(message)
-	case moveReady:
-		rb.Move(message)
-	}
-}
+// 	switch rb.state {
+// 	case paintReady:
+// 		rb.Paint(message)
+// 	case moveReady:
+// 		rb.Move(message)
+// 	}
+// }
 
-func (rb *Robot) Turn(direction int) (*Vector, bool) {
-	if v, ok := directions[*rb.direction]; ok {
-		switch direction {
-		case 0:
-			rb.direction.Flip()
-			rb.direction.x = rb.direction.x * v.x
-			rb.direction.y = rb.direction.y * v.x
-		case 1:
-			rb.direction.Flip()
-			rb.direction.x = rb.direction.x * v.y
-			rb.direction.y = rb.direction.y * v.y
-		}
-		return rb.direction, ok
-	}
-	return rb.direction, false
-}
+// func (rb *Robot) Turn(direction int) (*Vector, bool) {
+// 	if v, ok := directions[*rb.direction]; ok {
+// 		switch direction {
+// 		case 0:
+// 			rb.direction.Flip()
+// 			rb.direction.x = rb.direction.x * v.x
+// 			rb.direction.y = rb.direction.y * v.x
+// 		case 1:
+// 			rb.direction.Flip()
+// 			rb.direction.x = rb.direction.x * v.y
+// 			rb.direction.y = rb.direction.y * v.y
+// 		}
+// 		return rb.direction, ok
+// 	}
+// 	return rb.direction, false
+// }
 
-func initialize() *Robot {
-	rb := new(Robot)
-	rb.direction = new(Vector)
-	rb.location = new(Vector)
-	panel := new(Panel)
-	rb.panels = make(map[Panel]int)
-	rb.messages = make([]int, 0)
-	rb.location.x = 0
-	rb.location.y = 0
-	rb.direction.x = 0
-	rb.direction.y = 1
-	panel.x = rb.location.x
-	panel.y = rb.location.y
-	rb.panels[*panel] = 0
-	return rb
-}
+// func initialize() *Robot {
+// 	rb := new(Robot)
+// 	rb.direction = new(Vector)
+// 	rb.location = new(Vector)
+// 	panel := new(Panel)
+// 	rb.panels = make(map[Panel]int)
+// 	rb.messages = make([]int, 0)
+// 	rb.location.x = 0
+// 	rb.location.y = 0
+// 	rb.direction.x = 0
+// 	rb.direction.y = 1
+// 	panel.x = rb.location.x
+// 	panel.y = rb.location.y
+// 	rb.panels[*panel] = 0
+// 	return rb
+// }
 
-func PartI() {
-	puzzleInput := "day-11-input.txt"
-	rb := initialize()
-	vm := new(ivm.VM)
-	vm.Setup(puzzleInput, rb.Feed, rb.Receive)
-	exec := rb.Execute(vm)
-	exec()
-	fmt.Println(len(rb.panels))
-}
+// func PartI() {
+// 	puzzleInput := "day-11-input.txt"
+// 	rb := initialize()
+// 	vm := new(ivm.VM)
+// 	vm.Setup(puzzleInput, rb.Feed, rb.Receive)
+// 	exec := rb.Execute(vm)
+// 	exec()
+// 	fmt.Println(len(rb.panels))
+// }
 
-func main() {
-	PartI()
-}
+// func main() {
+// 	PartI()
+// }
