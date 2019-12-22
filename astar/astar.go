94e5427d28bbd05f77c993980c795dac00669f3b
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-22 19:12:43 +0530

    AStar heuristics

--- astar/astar.go
+++ astar/astar.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //Node is used to store grid data for A*
 type Node struct {
@@ -30,6 +33,23 @@ func (a *AStar) Populate(gsize int) {
 	}
 }
 
+//GetDistance calculates the manhattan distance to the destination
+func (a *AStar) GetDistance(n Node) int {
+	return int(math.Abs(float64(n.x-a.destination.x)) +
+		math.Abs(float64(n.y-a.destination.y)))
+}
+
+//calcH calculates the heuristics for the given grid
+func (a *AStar) calcH() {
+	mhd := 0
+	for i := 0; i < len(a.grid); i++ {
+		for j := 0; j < len(a.grid); j++ {
+			mhd = a.GetDistance(a.grid[i][j])
+			a.grid[i][j].h = mhd
+		}
+	}
+}
+
 //SetOrigin sets the starting point of the grid
 func (a *AStar) SetOrigin(x, y int) {
 	a.origin = &a.grid[y][x]
@@ -44,9 +64,15 @@ func (a *AStar) SetDestination(x, y int) {
 func (a *AStar) PrintGrid() {
 	for i := 0; i < len(a.grid); i++ {
 		for j := 0; j < len(a.grid); j++ {
-			fmt.Print(" . ")
-			if j == 9 {
+			if i == a.origin.x && j == a.origin.y {
+				fmt.Print(fmt.Sprintf(" %2s ", "o"))
+			} else if i == a.destination.x && j == a.destination.y {
+				fmt.Print(fmt.Sprintf(" %2s ", "x"))
+			} else if j == 9 {
 				fmt.Print("\n")
+			} else {
+				//fmt.Print(" . ")
+				fmt.Print(fmt.Sprintf(" %2d ", a.grid[i][j].h))
 			}
 		}
 	}
@@ -55,5 +81,8 @@ func (a *AStar) PrintGrid() {
 func main() {
 	a := AStar{}
 	a.Populate(10)
+	a.SetOrigin(1, 1)
+	a.SetDestination(6, 8)
+	a.calcH()
 	a.PrintGrid()
 }
