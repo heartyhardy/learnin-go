a949ab59d1b6ff2fdd6b78a18c05f280d9008727
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-28 22:41:39 +0530

    Graph AddVertex and AddEdge

--- graphs/graph-basic.go
+++ graphs/graph-basic.go
@@ -13,10 +13,19 @@ type Graph struct {
 
 //AddVertex ...
 func (g *Graph) AddVertex(v int) {
-
+	if _, ok := g.matrix[v]; !ok {
+		g.matrix[v] = make([]int, 0)
+	}
 }
 
 //AddEdge ...
-func (g *Graph) AddEdge(e int) {
-
+func (g *Graph) AddEdge(v1 int, v2 int) {
+	if _, ok := g.matrix[v1]; !ok {
+		g.AddVertex(v1)
+	}
+	if _, ok := g.matrix[v2]; !ok {
+		g.AddVertex(v2)
+	}
+	g.matrix[v1] = append(g.matrix[v1], v2)
+	g.matrix[v2] = append(g.matrix[v2], v1)
 }
