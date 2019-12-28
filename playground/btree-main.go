9ec3d614f95b2a027caddf89f706ccc9273ace13
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-28 17:50:10 +0530

    Contains duplicate ii

--- playground/btree-main.go
+++ playground/btree-main.go
@@ -1,21 +1,17 @@
 package main
 
-import (
-	"heartyhardy/trees/btree"
-)
+// func main() {
+// 	bst := btree.BinarySearchTree{}
+// 	bst.Insert(5)
+// 	bst.Insert(4)
+// 	bst.Insert(10)
+// 	bst.Insert(11)
+// 	bst.Insert(12)
+// 	bst.Insert(3)
 
-func main() {
-	bst := btree.BinarySearchTree{}
-	bst.Insert(5)
-	bst.Insert(4)
-	bst.Insert(10)
-	bst.Insert(11)
-	bst.Insert(12)
-	bst.Insert(3)
-
-	//x := bst.Lookup(5)
-	//fmt.Println(x.Value)
-	//bst.Root.Print(0, "root")
-	bst.Remove(10)
-	bst.Root.Print(0, "root")
-}
+// 	//x := bst.Lookup(5)
+// 	//fmt.Println(x.Value)
+// 	//bst.Root.Print(0, "root")
+// 	bst.Remove(10)
+// 	bst.Root.Print(0, "root")
+// }
---
+++ playground/duplicates-ij.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	hm := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		if val, ok := hm[nums[i]]; ok && i-val <= k {
+			return true
+		} else {
+			hm[nums[i]] = i
+		}
+	}
+	return false
+}
+
+func main() {
+	a := []int{1, 0, 1, 1}
+	res := containsNearbyDuplicate(a, 1)
+	fmt.Println(res)
+}
