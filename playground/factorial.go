18fa1698da683ee94bab1189ee4e6f17a1f25588
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-12-31 11:42:24 +0530

    Reverse string recursive

--- playground/factorial.go
+++ playground/factorial.go
@@ -17,10 +17,19 @@ func fibonacci(n int) int {
 	return fibonacci(n-1) + fibonacci(n-2)
 }
 
+func reverseString(s string) string {
+	if s == "" {
+		return s
+	}
+	return reverseString(s[1:]) + string(s[0])
+}
+
 func main() {
 	res := factorial(5)
 	fmt.Println("Factorial", res)
 
 	res = fibonacci(6)
 	fmt.Println("Fibonacci", res)
+
+	fmt.Println(reverseString("hello world"))
 }
--- playground/minimum-time.go
+++ playground/minimum-time.go
@@ -1,28 +1,26 @@
 package main
 
-// import "fmt"
+func minTimeToVisitAllPoints(points [][]int) int {
+	var xdiff, ydiff, time int
+	for i := 0; i < len(points)-1; i++ {
+		if xdiff = points[i+1][0] - points[i][0]; xdiff < 0 {
+			xdiff = -xdiff
+		}
+		if ydiff = points[i+1][1] - points[i][1]; ydiff < 0 {
+			ydiff = -ydiff
+		}
 
-// func minTimeToVisitAllPoints(points [][]int) int {
-// 	var xdiff, ydiff, time int
-// 	for i := 0; i < len(points)-1; i++ {
-// 		if xdiff = points[i+1][0] - points[i][0]; xdiff < 0 {
-// 			xdiff = -xdiff
-// 		}
-// 		if ydiff = points[i+1][1] - points[i][1]; ydiff < 0 {
-// 			ydiff = -ydiff
-// 		}
-
-// 		switch {
-// 		case xdiff > ydiff:
-// 			time += xdiff
-// 		case xdiff < ydiff:
-// 			time += ydiff
-// 		case xdiff == ydiff:
-// 			time += xdiff
-// 		}
-// 	}
-// 	return time
-// }
+		switch {
+		case xdiff > ydiff:
+			time += xdiff
+		case xdiff < ydiff:
+			time += ydiff
+		case xdiff == ydiff:
+			time += xdiff
+		}
+	}
+	return time
+}
 
 // func main() {
 // 	//a := [][]int{[]int{1, 1}, []int{3, 4}, []int{-1, 0}}
