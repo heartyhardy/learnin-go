694940a2544b333ee43d518aaa1326fc73c59aa9
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-01 02:23:27 +0530

    Bubble sort descending

--- playground/bubble-sort.go
+++ playground/bubble-sort.go
@@ -23,8 +23,33 @@ func bubblesort(nums []int) {
 	}
 }
 
+func bubblesortDesc(nums []int) {
+	i, l, swp := 0, len(nums)-1, false
+	for {
+		if nums[i] < nums[i+1] {
+			t := nums[i]
+			nums[i] = nums[i+1]
+			nums[i+1] = t
+			swp = true
+		}
+		if i == l-1 && swp {
+			l--
+			i = 0
+			swp = false
+			continue
+		} else if i == l && !swp {
+			break
+		}
+		i++
+	}
+}
+
 func main() {
 	a := []int{1, 6, 3, 7, 2, 5, 4, 9, 8, 0}
 	bubblesort(a)
 	fmt.Println("Bubble sorted: ", a)
+
+	a = []int{1, 6, 3, 7, 2, 5, 4, 9, 8, 0}
+	bubblesortDesc(a)
+	fmt.Println("Bubblesort Desc: ", a)
 }
