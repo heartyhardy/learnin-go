433652ed2c68d2218b4236466760610782396276
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-02 00:36:06 +0530

    Third Max

--- playground/third-max.go
+++ playground/third-max.go
@@ -1,46 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func thirdMax(nums []int) int {
-	swp := false
-	res := map[int]int{}
-	pos, third, l := 0, 0, len(nums)-1
-	for i := 0; i < l; i++ {
-		if nums[i] < nums[i+1] {
-			nums[i], nums[i+1] = nums[i+1], nums[i]
-			swp = true
-		}
-		if i == l-1 && !swp {
-			break
-		} else if i == l-1 && swp {
-			l--
-			i = -1
-			swp = false
-			continue
+	max := make(map[int]int)
+	keys := []int{}
+	for i := 0; i < len(nums); i++ {
+		if _, ok := max[nums[i]]; !ok {
+			max[nums[i]] = nums[i]
+			keys = append(keys, nums[i])
 		}
 	}
+	sort.Ints(keys)
+	fmt.Println(keys, max)
 
-	for i := 0; i < len(nums) && pos < 3; i++ {
-		if _, ok := res[nums[i]]; !ok {
-			res[nums[i]] = nums[i]
-			third = nums[i]
-			pos++
-		}
+	if len(max) < 3 {
+		return keys[len(keys)-1]
 	}
-
-	if len(res) <= 2 {
-		for _, v := range res {
-			if third < v {
-				third = v
-			}
-		}
-	}
-	return third
+	keys = keys[len(keys)-3 : len(keys)-1]
+	return keys[0]
 }
 
 func main() {
-	a := []int{1, -1, 3, -2, 3}
+	a := []int{1, 0}
 	res := thirdMax(a)
 	fmt.Println("Zeroes moved: ", res)
 }
