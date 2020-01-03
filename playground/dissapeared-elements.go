46d0c2d870751b7f62d99ef008f343478e563c13
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-03 18:11:17 +0530

    Dissapeared Numbers final solution

--- playground/dissapeared-elements.go
+++ playground/dissapeared-elements.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // func findDisappearedNumbers(nums []int) []int {
 // 	uq := make(map[int]int)
@@ -18,16 +21,45 @@ import "fmt"
 // 	return res
 // }
 
-func findDisappearedNumbers(nums []int) []int {
-	all := make([]int, len(nums)+1)
-	res := make([]int, 0)
-	for i := 0; i < len(nums); i++ {
-		all[nums[i]] = nums[i]
-	}
+// func findDisappearedNumbers(nums []int) []int {
+// 	all := make([]int, len(nums)+1)
+// 	res := make([]int, 0)
+// 	for i := 0; i < len(nums); i++ {
+// 		all[nums[i]] = nums[i]
+// 	}
 
-	for i := 1; i < len(all); i++ {
-		if all[i] == 0 {
+// 	for i := 1; i < len(all); i++ {
+// 		if all[i] == 0 {
+// 			res = append(res, i)
+// 		}
+// 	}
+// 	return res
+// }
+
+func findDisappearedNumbersSmall(nums []int) []int {
+	res := make([]int, 0)
+	nsum := 0
+	for i := 0; i < len(nums); i++ {
+		nsum |= int(math.Pow(2, float64(nums[i])))
+	}
+	for i := 1; i < len(nums)+1; i++ {
+		if nsum&int(math.Pow(2, float64(i))) == 0 {
 			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func findDisappearedNumbers(nums []int) []int {
+
+	l := len(nums)
+	res := make([]int, 0)
+	for _, v := range nums {
+		nums[(v-1)%l] += l
+	}
+	for i, v := range nums {
+		if v <= l {
+			res = append(res, i+1)
 		}
 	}
 	return res
