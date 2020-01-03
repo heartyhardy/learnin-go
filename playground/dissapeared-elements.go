90fde0c069a47570fdb3b503ac76ee5ff0f03e08
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-03 15:44:23 +0530

    Dissapeared elements 2nd algo

--- playground/dissapeared-elements.go
+++ playground/dissapeared-elements.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
+// func findDisappearedNumbers(nums []int) []int {
+// 	uq := make(map[int]int)
+// 	res := make([]int, 0)
+// 	for i := 0; i < len(nums); i++ {
+// 		if _, ok := uq[nums[i]]; !ok {
+// 			uq[nums[i]] = nums[i]
+// 		}
+// 	}
+// 	for i := 1; i < len(nums)+1; i++ {
+// 		if _, ok := uq[i]; !ok {
+// 			res = append(res, i)
+// 		}
+// 	}
+// 	return res
+// }
+
 func findDisappearedNumbers(nums []int) []int {
-	uq := make(map[int]int)
+	all := make([]int, len(nums)+1)
 	res := make([]int, 0)
 	for i := 0; i < len(nums); i++ {
-		if _, ok := uq[nums[i]]; !ok {
-			uq[nums[i]] = nums[i]
-		}
+		all[nums[i]] = nums[i]
 	}
-	for i := 1; i < len(nums)+1; i++ {
-		if _, ok := uq[i]; !ok {
+
+	for i := 1; i < len(all); i++ {
+		if all[i] == 0 {
 			res = append(res, i)
 		}
 	}
@@ -19,7 +34,7 @@ func findDisappearedNumbers(nums []int) []int {
 }
 
 func main() {
-	a := []int{1, 3, 3, 5, 5}
+	a := []int{4, 3, 2, 7, 8, 2, 3, 1}
 	res := findDisappearedNumbers(a)
 	fmt.Println(res)
 }
