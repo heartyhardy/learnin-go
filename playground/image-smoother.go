15de93195e713e9db4110df98d25d24b6a644cf2
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-10 08:48:38 +0530

    Image Smoother

--- playground/image-smoother.go
+++ playground/image-smoother.go
@@ -3,18 +3,52 @@ package main
 import "fmt"
 
 func imageSmoother(M [][]int) [][]int {
-	pos := 0
-	tmp := make([]int, len(M)*len(M[0]))
-
-	for _, r := range M {
-		for _, c := range r {
-			tmp[pos] = c
-			pos++
-		}
+	// type xy struct {
+	// 	x int
+	// 	y int
+	// }
+	// xymap := map[uint8]xy{
+	// 	0: {0, 0},
+	// 	1: {-1, -1},
+	// 	2: {0, -1},
+	// 	3: {1, -1},
+	// 	4: {-1, 0},
+	// 	5: {1, 0},
+	// 	6: {-1, 1},
+	// 	7: {0, 1},
+	// 	8: {1, 1},
+	// }
+	xymap := [][]int{
+		[]int{0, 0},
+		[]int{-1, -1},
+		[]int{0, -1},
+		[]int{1, -1},
+		[]int{-1, 0},
+		[]int{1, 0},
+		[]int{-1, 1},
+		[]int{0, 1},
+		[]int{1, 1},
+	}
+	res := make([][]int, len(M))
+	for i, rv := range M {
+		res[i] = make([]int, len(rv))
 	}
 
-	fmt.Println(tmp)
-	return M
+	rlen, clen := len(M), len(M[0])
+	for r, rv := range M {
+		for c := range rv {
+			cells, cavg := 0, 0
+			for _, v := range xymap {
+				cx, cy := c+v[0], r+v[1]
+				if cx >= 0 && cx < clen && cy >= 0 && cy < rlen {
+					cavg += M[cy][cx]
+					cells++
+				}
+			}
+			res[r][c] = cavg / cells
+		}
+	}
+	return res
 }
 
 func main() {
