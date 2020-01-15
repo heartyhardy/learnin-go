02c1127c81334ebb4eb04f5a722fbba594ad5b15
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-15 22:31:38 +0530

    AoC 2015 - Day - 07 Parsing

--- aoc/aoc15/day-07.go
+++ aoc/aoc15/day-07.go
@@ -1,9 +1,10 @@
 package aoc15
 
 import (
-	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 const (
@@ -12,6 +13,7 @@ const (
 	not
 	rshift
 	lshift
+	eq
 )
 
 type store struct {
@@ -21,15 +23,14 @@ type store struct {
 
 type wire struct {
 	v      int
-	origin gate
-	gates  map[string]gate
+	origin string
 }
 
 type gate struct {
 	v      int
 	lin    string
 	rin    string
-	out    int
+	out    string
 	op     int
 	solved bool
 }
@@ -43,14 +44,59 @@ func readLines(filename string) (content []string) {
 	return
 }
 
-func parseFunctions(content []string) {
-	for _, v := range content {
+func parseLine(ds *store, line string) {
+	fields := strings.FieldsFunc(line, func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	})
 
+	w := wire{origin: line}
+	fn := gate{}
+
+	switch len(fields) {
+	case 2:
+		if v, err := strconv.Atoi(fields[0]); err == nil {
+			w.v = v
+		} else {
+			fn.lin = fields[0]
+			fn.out = fields[1]
+			fn.op = eq
+
+		}
+	case 3:
+		fn.op = not
+		fn.lin = fields[1]
+		fn.out = fields[2]
+	case 4:
+		switch fields[1] {
+		case "AND":
+			fn.op = and
+		case "OR":
+			fn.op = or
+		case "LSHIFT":
+			fn.op = lshift
+		case "RSHIFT":
+			fn.op = rshift
+		}
+		fn.lin = fields[0]
+		fn.rin = fields[2]
+		fn.out = fields[3]
 	}
+	ds.wires[fields[len(fields)-1]] = w
+	ds.gates[line] = fn
+}
+
+func parseAll(content []string) (ds *store) {
+	ds = new(store)
+	ds.wires = make(map[string]wire)
+	ds.gates = make(map[string]gate)
+	for _, v := range content {
+		parseLine(ds, v)
+	}
+	return
 }
 
 //Run solution
 func Run() {
 	content := readLines("./inputs/day-07.txt")
-	fmt.Println(content)
+	parseAll(content)
 }
