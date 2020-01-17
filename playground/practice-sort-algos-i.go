a5ecc119bc048850e4fc17d3fbd45b4f6f0ba037
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-17 08:23:40 +0530

    Practice round: Sorting 2020 01017

--- playground/practice-sort-algos-i.go
+++ playground/practice-sort-algos-i.go
@@ -1,13 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func bubblesort20200116(nums []int) []int {
 	swapped := false
 	i, imax, swpn := 0, len(nums)-1, 0
-	steps := 0
 	for i < imax {
-		steps++
 		if nums[i] > nums[i+1] {
 			nums[i], nums[i+1] = nums[i+1], nums[i]
 			swapped = true
@@ -22,12 +23,78 @@ func bubblesort20200116(nums []int) []int {
 		}
 		i++
 	}
-	fmt.Println(steps)
 	return nums
+}
+
+func selectionSort20200117(nums []int) []int {
+	min := 0
+	for i := 0; i < len(nums); i++ {
+		min = i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[min] > nums[j] {
+				min = j
+			}
+		}
+		nums[i], nums[min] = nums[min], nums[i]
+	}
+	return nums
+}
+
+func shuffle(nums []int) {
+	src := rand.NewSource(100)
+	rnd := rand.New(src)
+	for i := 0; i < len(nums); i++ {
+		idx := rnd.Intn(len(nums) - 1)
+		nums[i], nums[idx] = nums[idx], nums[i]
+	}
+	fmt.Println("\nShuffled: ", nums)
+}
+
+func mergesort20200117(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+	m := len(nums) / 2
+	return merge20200117(mergesort20200117(nums[:m]), mergesort20200117(nums[m:]))
+}
+
+func merge20200117(l, r []int) []int {
+	size, i, j := len(l)+len(r), 0, 0
+	res := make([]int, len(l)+len(r))
+
+	for k := 0; k < size; k++ {
+		switch {
+		case i == len(l):
+			res[k] = r[j]
+			j++
+		case j == len(r):
+			res[k] = l[i]
+			i++
+		case l[i] < r[j]:
+			res[k] = l[i]
+			i++
+		case l[i] > r[j]:
+			res[k] = r[j]
+			j++
+		case l[i] == r[j]:
+			res[k] = r[j]
+			j++
+		}
+	}
+	return res
 }
 
 func main() {
 	nums := []int{1, 5, 3, 2, 7, 5, 7, 9, 6, 11, 10}
 	res := bubblesort20200116(nums)
+	fmt.Println("\n -- Bubble sort --")
+	fmt.Println(res)
+	shuffle(nums)
+	res = selectionSort20200117(nums)
+	fmt.Println("\n -- Selection sort --")
+	fmt.Println(res)
+	shuffle(nums)
+	res = mergesort20200117(nums)
+	fmt.Println("\n -- Merge sort --")
 	fmt.Println(res)
 }
