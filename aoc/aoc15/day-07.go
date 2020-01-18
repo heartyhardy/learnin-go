592c7f02bd085a5a2542b3500823b084e7055d9c
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-18 23:53:00 +0530

    Yet to implement Recursive seek and backtracking

--- aoc/aoc15/day-07.go
+++ aoc/aoc15/day-07.go
@@ -1,7 +1,6 @@
 package aoc15
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -17,6 +16,8 @@ const (
 	eq
 )
 
+type gates map[string][]string
+
 func readLines(filename string) (content []string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -26,7 +27,7 @@ func readLines(filename string) (content []string) {
 	return
 }
 
-func mapFunctions(content ...string) (fn map[string][]string) {
+func mapFunctions(content ...string) (fn gates) {
 	fn = make(map[string][]string)
 	for _, line := range content {
 		fields := strings.FieldsFunc(line, func(c rune) bool {
@@ -52,11 +53,106 @@ func mapFunctions(content ...string) (fn map[string][]string) {
 	return
 }
 
+func resolve(fn gates) {
+	values := make(map[string]uint16)
+	for k, v := range fn {
+		// fmt.Println(k, v)
+		// fmt.Println(sv, isVal, path)
+		// fmt.Println(fn[path[len(path)-1]])
+		path := make([]string, 0)
+		fv, isSolved := canSolve(values, v)
+		if isSolved {
+			values[v[4]] = fv
+			continue
+		} else {
+
+		}
+		break
+	}
+}
+
+func canSolve(values map[string]uint16, fnops []string) (val uint16, solved bool) {
+
+	if len(fnops[0]) == 0 {
+		vfn, err := strconv.ParseUint(fnops[3], 10, 16)
+		if err != nil {
+			if v, ok := values[fnops[1]]; !ok {
+				return 0, false
+			} else {
+				val = v
+				solved = true
+				return
+			}
+		}
+		val = uint16(vfn)
+		solved = true
+		return
+	}
+
+	vl, lerr := strconv.ParseUint(fnops[1], 10, 16)
+	vr, rerr := strconv.ParseUint(fnops[2], 10, 16)
+	var l, r uint16
+
+	if lerr != nil {
+		if v, ok := values[fnops[1]]; ok {
+			l = v
+		} else {
+			return 0, false
+		}
+	} else {
+		l = uint16(vl)
+	}
+	if rerr != nil {
+		if v, ok := values[fnops[1]]; ok {
+			r = v
+		} else {
+			return 0, false
+		}
+	} else {
+		r = uint16(vr)
+	}
+
+	switch fnops[0] {
+	case "NOT":
+		val = ^l
+	case "AND":
+		val = l & r
+	case "OR":
+		val = l | r
+	}
+	solved = true
+	return
+}
+
+func seek(fn string, fns gates, path *[]string) (v uint16) {
+	ops := fns[fn]
+
+	switch ops[0] {
+	case "":
+		//Save path
+		//Save value
+		//Return
+	case "NOT":
+		//Save path
+		//Seek (L)
+	case "AND":
+		fallthrough
+	case "OR":
+		fallthrough
+	case "LSHIFT":
+		fallthrough
+	case "RSHIFT":
+		//Convert L  and R
+		// If R is a Value and L is not -> Select L
+		// If L is a Value and R is not -> Select R
+		// If both are not Values, Select L
+	}
+}
+
 //Run solution
 func Run() {
 	content := readLines("./inputs/day-07.txt")
 	fn := mapFunctions(content...)
-	for k, v := range fn {
-		fmt.Println(k, v)
-	}
+	resolve(fn)
+
 }
