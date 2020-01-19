a5a213fcc290eb87155b2cedd483d29a572168aa
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-19 14:24:02 +0530

    AoC 2015 - Day - 7

--- aoc/aoc15/day-07.go
+++ aoc/aoc15/day-07.go
@@ -1,6 +1,7 @@
 package aoc15
 
 import (
+	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -17,6 +18,8 @@ const (
 )
 
 type gates map[string][]string
+type links map[string][]string
+type values map[string]uint16
 
 func readLines(filename string) (content []string) {
 	data, err := ioutil.ReadFile(filename)
@@ -27,8 +30,20 @@ func readLines(filename string) (content []string) {
 	return
 }
 
-func mapFunctions(content ...string) (fn gates) {
-	fn = make(map[string][]string)
+func replaceAndReadLines(filename string, a string, b string) (content []string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic("Error reading file !")
+	}
+	newContent := strings.Replace(string(data), a, b, 1)
+	content = strings.Split(newContent, "\n")
+	return
+}
+
+func mapFunctions(content ...string) (fn gates, cns links, vns values) {
+	fn = make(gates)
+	cns = make(links)
+	vns = make(values)
 	for _, line := range content {
 		fields := strings.FieldsFunc(line, func(c rune) bool {
 			return !unicode.IsLetter(c) && !unicode.IsNumber(c)
@@ -38,121 +53,120 @@ func mapFunctions(content ...string) (fn gates) {
 		switch len(fields) {
 		case 2:
 			fops[4] = fields[len(fields)-1]
-			if _, err := strconv.Atoi(fields[0]); err != nil {
-				fops[1] = fields[0]
-			} else {
+
+			if v, isNum := isNumeric(fields[0]); isNum {
 				fops[3] = fields[0]
+				vns[fops[4]] = v
+			} else {
+				fops[0], fops[1] = "EQ", fields[0]
+				mapConnections(fops[4], cns, fops[4], fops[1])
 			}
 		case 3:
 			fops[0], fops[1], fops[4] = fields[0], fields[1], fields[len(fields)-1]
+			mapConnections(fops[4], cns, fops[4], fops[1])
 		case 4:
 			fops[0], fops[1], fops[2], fops[4] = fields[1], fields[0], fields[2], fields[len(fields)-1]
+			mapConnections(fops[4], cns, fops[4], fops[1], fops[2])
 		}
 		fn[fields[len(fields)-1]] = fops
 	}
 	return
 }
 
-func resolve(fn gates) {
-	values := make(map[string]uint16)
-	for k, v := range fn {
-		// fmt.Println(k, v)
-		// fmt.Println(sv, isVal, path)
-		// fmt.Println(fn[path[len(path)-1]])
-		path := make([]string, 0)
-		fv, isSolved := canSolve(values, v)
-		if isSolved {
-			values[v[4]] = fv
-			continue
-		} else {
-
+func mapConnections(w string, cns links, ops ...string) {
+	for _, op := range ops {
+		if _, isNum := isNumeric(op); !isNum && len(op) > 0 {
+			if _, ok := cns[op]; !ok {
+				cns[op] = make([]string, 0)
+			}
+			cns[op] = append(cns[op], w)
 		}
-		break
 	}
 }
 
-func canSolve(values map[string]uint16, fnops []string) (val uint16, solved bool) {
+func trySolve(fns gates, cns links, vns values) {
 
-	if len(fnops[0]) == 0 {
-		vfn, err := strconv.ParseUint(fnops[3], 10, 16)
-		if err != nil {
-			if v, ok := values[fnops[1]]; !ok {
-				return 0, false
-			} else {
-				val = v
-				solved = true
-				return
+	isSolved := func(w string) bool {
+		if ops, ok := fns[w]; ok {
+			switch ops[0] {
+			case "":
+				_, lok := tryFetchValue(ops[3], vns)
+				return lok
+			case "EQ":
+				l, lok := tryFetchValue(ops[1], vns)
+				if lok {
+					vns[ops[4]] = l
+					return lok
+				}
+			case "NOT":
+				l, lok := tryFetchValue(ops[1], vns)
+				if _, exists := vns[ops[4]]; !exists && lok {
+					vns[ops[4]] = ^l
+					return lok
+				}
+			default:
+				l, lok := tryFetchValue(ops[1], vns)
+				r, rok := tryFetchValue(ops[2], vns)
+				if lok && rok {
+					switch ops[0] {
+					case "OR":
+						vns[ops[4]] = l | r
+					case "AND":
+						vns[ops[4]] = l & r
+					case "LSHIFT":
+						vns[ops[4]] = l << r
+					case "RSHIFT":
+						vns[ops[4]] = l >> r
+					}
+					return true
+				}
 			}
 		}
-		val = uint16(vfn)
-		solved = true
-		return
+		return false
 	}
 
-	vl, lerr := strconv.ParseUint(fnops[1], 10, 16)
-	vr, rerr := strconv.ParseUint(fnops[2], 10, 16)
-	var l, r uint16
-
-	if lerr != nil {
-		if v, ok := values[fnops[1]]; ok {
-			l = v
-		} else {
-			return 0, false
+	for len(vns) < len(fns) {
+		for w := range vns {
+			for _, l := range cns[w] {
+				_ = isSolved(l)
+			}
 		}
-	} else {
-		l = uint16(vl)
 	}
-	if rerr != nil {
-		if v, ok := values[fnops[1]]; ok {
-			r = v
-		} else {
-			return 0, false
-		}
-	} else {
-		r = uint16(vr)
-	}
-
-	switch fnops[0] {
-	case "NOT":
-		val = ^l
-	case "AND":
-		val = l & r
-	case "OR":
-		val = l | r
-	}
-	solved = true
-	return
 }
 
-func seek(fn string, fns gates, path *[]string) (v uint16) {
-	ops := fns[fn]
-
-	switch ops[0] {
-	case "":
-		//Save path
-		//Save value
-		//Return
-	case "NOT":
-		//Save path
-		//Seek (L)
-	case "AND":
-		fallthrough
-	case "OR":
-		fallthrough
-	case "LSHIFT":
-		fallthrough
-	case "RSHIFT":
-		//Convert L  and R
-		// If R is a Value and L is not -> Select L
-		// If L is a Value and R is not -> Select R
-		// If both are not Values, Select L
+func isNumeric(w string) (uint16, bool) {
+	v, err := strconv.ParseUint(w, 10, 16)
+	if err != nil {
+		return 0, false
 	}
+	return uint16(v), true
+}
+
+func tryFetchValue(w string, vns values) (val uint16, ok bool) {
+	if v, isNum := isNumeric(w); isNum {
+		return uint16(v), true
+	} else if !isNum && len(w) > 0 {
+		if vl, ok := vns[w]; ok {
+			return vl, true
+		}
+		return 0, false
+	}
+	return 0, false
 }
 
 //Run solution
 func Run() {
 	content := readLines("./inputs/day-07.txt")
-	fn := mapFunctions(content...)
-	resolve(fn)
+	fn, cns, vns := mapFunctions(content...)
+	trySolve(fn, cns, vns)
+	sigA, sigB := strconv.Itoa(int(vns["a"])), strconv.Itoa(int(vns["b"]))
 
+	content = replaceAndReadLines("./inputs/day-07.txt", sigB, sigA)
+	fn, cns, vns = mapFunctions(content...)
+	trySolve(fn, cns, vns)
+
+	fmt.Println("\n-- AoC 2015: Day 7: Some Assembly Required --")
+	fmt.Printf("\n ⚡ :(Wire A): %v \n ⚡⚡:(Wire A Override): %v", sigA, vns["a"])
+	fmt.Println("\n\n-- DONE --")
+	fmt.Println("")
 }
